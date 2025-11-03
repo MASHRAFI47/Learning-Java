abd998ba7634682a6f9a5f9d3a27a916f39b138c
Mashrafi Bin Nur <mbngms@gmail.com>
2025-11-03 09:06:16 +0600

    Main change

--- AgoraShop/Main.java
+++ AgoraShop/Main.java
@@ -1,8 +1,14 @@
 package AgoraShop;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Agora agora = new Agora("mbngms@gmail.com", "Mirpur");
+        Scanner sc = new Scanner(System.in);
+        String email = sc.nextLine();
+        String location = sc.nextLine();
+
+        Agora agora = new Agora(email, location);
 
         Book b1 = new Book(25.69, 55512);
         Book b2 = new Book(42.69, 15617);
